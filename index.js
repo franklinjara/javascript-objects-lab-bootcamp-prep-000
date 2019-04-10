2f355fc2062efbcb769ac02aa8a628136603dbe4
Franklin <makyfj@hotmail.com>
2019-04-10 17:16:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {};
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, object)
   
-  return newObj;
+  return object;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
